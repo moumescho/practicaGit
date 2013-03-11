68a294d7e3f80bfff23fdb5d38d667e7d2bd76ff
Varuiza <victor.ruiz.alcala@gmail.com>
2013-03-11 22:31:10 +0100

    Cambios Alumno 2

--- GitAux.java
+++ GitAux.java
@@ -16,9 +16,12 @@ public class GitAux {
     
     public void metodoAlumno2(){
         System.out.println("metodo 2");
+        System.out.println("modificado por alumno 2"); // alumno 2
     }
     
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
+	// modificaciones alumno 2
+        System.out.println("Alumno 2 inserta esta sentencia");
     }
 }
